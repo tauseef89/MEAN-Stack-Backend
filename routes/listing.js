6a0a1506f36fb2a1ed5556b3c39df2766463d452
tauseef89 <tauseef.n89@gmail.com>
2019-12-26 14:53:44 +0530

    new routes added

--- routes/listing.js
+++ routes/listing.js
@@ -1,7 +1,30 @@
 const router = require("express").Router();
 
+// Add New listing
+
+router.post("/", (req, res) => {
+  res.send("Add New listing");
+});
+
+// Get All listings
+
 router.get("/", (req, res) => {
   res.send("All listings");
+});
+
+// Single listing
+router.get("/:listingId", (req, res) => {
+  res.send("Single listing");
+});
+
+// Update listing
+router.put("/:listingId", (req, res) => {
+  res.send("Update listing");
+});
+
+// Delete listing
+router.delete("/:listingId", (req, res) => {
+  res.send("Delete listing");
 });
 
 module.exports = router;
