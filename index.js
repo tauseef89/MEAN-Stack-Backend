b4a6cc6d5c838ac9ec9f26f857b95fd5657945e5
tauseef89 <tauseef.n89@gmail.com>
2019-12-26 15:19:43 +0530

    mongo db connection added

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+// connect to db
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("connected to db")
+);
 
 // Import routes
 const listinRoutes = require("./routes/listing");
