e5885746591e90a6b342aa5338d6de4b69d0f6b3
tauseef89 <tauseef.n89@gmail.com>
2020-01-03 18:47:57 +0530

    part 8 user login

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   // checking user email id in database
@@ -27,6 +28,21 @@ router.post("/register", async (req, res) => {
   } catch (error) {
     res.status(400).send(error);
   }
+});
+
+// user login
+router.post("/login", async (req, res) => {
+  // checking user email id in database
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email is wrong");
+
+  // checking password
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid password");
+
+  // creat and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token).send({ token: token });
 });
 
 module.exports = router;
