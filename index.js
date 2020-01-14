3c358a3900034ebe7d6037b0ecaff9a7fdfa9bfe
tauseef89 <tauseef.n89@gmail.com>
2020-01-14 16:47:37 +0530

    cors issue fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ const userRoutes = require("./routes/user");
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // route Middlewares
 app.use("/api/listings", listinRoutes);
